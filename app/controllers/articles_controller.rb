d20ae2d60c8d4a779ae617d8e8b664dcab2ceb30
J <kiendo95@gmail.com>
2016-10-10 02:39:08 +0700

    finish required

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,6 +5,7 @@ class ArticlesController < ApplicationController
   # GET /articles.json
   def index
     @articles = Article.all
+    @markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
   end
 
   # GET /articles/1
@@ -54,6 +55,7 @@ class ArticlesController < ApplicationController
   # DELETE /articles/1
   # DELETE /articles/1.json
   def destroy
+    @article = Article.find(params[:id])
     @article.destroy
     respond_to do |format|
       format.html { redirect_to articles_url, notice: 'Article was successfully destroyed.' }
